32cd2c642dda364e1ba017ddf87b9f3388c8bbb2
KwonKusang <rnjsrntkd95@gmail.com>
2020-02-04 22:49:20 +0900

    defined models

--- K-asa/models.py
+++ K-asa/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
+class Groups(models.Model):
+    gname = models.CharField(max_length=255, verbose_name='그룹이름')
+    agency = models.CharField(max_length=255, blank=True, verbose_name='소속사')
+    sns_url = models.URLField(verbose_name='SNS', blank=True)
+
+
+class Albums(models.Model):
+    release_date = models.DateField(verbose_name='발매일')
+    album_art = models.ImageField(verbose_name='자켓사진')
+    group = models.ForeignKey(Groups, on_delete=models.CASCADE, verbose_name='그룹이름', related_name='group_album')
+
+
+class Singers(models.Model):
+    sname = models.CharField(max_length=255, verbose_name='가수이름')
+    image = models.ImageField(blank=True, verbose_name='가수사진')
+    wiki_url = models.URLField(blank=True, verbose_name='가수정보')
+    group = models.ManyToManyField(Groups, verbose_name='소속그룹', related_name='group_singer')
+
+
+class Songs(models.Model):
+    youtube_url = models.URLField(blank=True, verbose_name='뮤직비디오')
+    soundcloud_url = models.URLField(blank=True, verbose_name='오디오')
+    dance_url = models.URLField(blank=True, verbose_name='무대영상')
+    album = models.ForeignKey(Albums, on_delete=models.CASCADE, verbose_name='앨범', related_name='album_song')
+    singer = models.ManyToManyField(Singers, verbose_name='가수', related_name='singer_song')
+
+
+class Comments(models.Model):
+    content = models.TextField(verbose_name='내용')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='작성일')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='작성자', related_name='user_comment')
+    song = models.ForeignKey(Songs, on_delete=models.CASCADE, verbose_name='노래', related_name='song_comment')
+
+
+class Fandoms(models.Model):
+    fname = models.CharField(max_length=255, verbose_name='팬덤명')
+    official_url = models.URLField(verbose_name='팬카페')
+    group = models.OneToOneField(Groups, on_delete=models.CASCADE, verbose_name='그룹', related_name='group_fandom')
+
+
+class Lyrics(models.Model):
+    kor = models.TextField(verbose_name='한국어')
+    eng = models.TextField(blank=True, verbose_name='영어')
+    rom = models.TextField(blank=True, verbose_name='로마자')
+    fanchant = models.TextField(blank=True, verbose_name='응원법')
+    order = models.IntegerField(verbose_name='순서')
+    song = models.ForeignKey(Songs, on_delete=models.CASCADE, verbose_name='노래', related_name='song_lyrics')
+    singer = models.ManyToManyField(Singers, verbose_name='파트', related_name='singer_lyrics')
+
+
+class Explanations(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='작성일')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='수정일')
+    content = models.TextField(verbose_name='내용')
+    image = models.ImageField(blank=True, verbose_name='사진')
+    likes = models.IntegerField(default=0, editable=False, verbose_name='좋아요')
+    lyrics = models.ForeignKey(Lyrics, on_delete=models.CASCADE, verbose_name='가사', related_name='lyrics_explanation')
--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,7 @@
-from django.contrib.auth.models import AbstractUser, UserManager
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.contrib.auth.models import UserManager as DefaultUserManager
 
 
 class User(AbstractUser):
     alias = models.CharField(max_length=255, unique=True, verbose_name='닉네임')
     interest = models.CharField(max_length=255, blank=True, verbose_name='관심사')
-
-
-
-UserManager
