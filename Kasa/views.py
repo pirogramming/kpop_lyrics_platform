7806bd7a2801354b06df89e9dc9d21e30df3691e
KwonKusang <rnjsrntkd95@gmail.com>
2020-02-10 16:55:41 +0900

    commit before merge

--- Kasa/views.py
+++ Kasa/views.py
@@ -7,12 +7,15 @@ from Kasa.models import Groups, Songs, Singers, Lyrics, Comments
 
 def detail_song(request, song_pk):
     song = get_object_or_404(Songs, pk=song_pk)
+    album = song.album
     sns = song.album.group.sns_url
     youtube_url = convert_youtube(song.youtube_url)
     all_lyrics = song.song_lyrics.all()
     comments = song.song_comment.all().order_by('-created_at')
+
     context = {
         'song': song,
+        'album': album,
         'comments': comments,
         'youtube_url': youtube_url,
         'sns': sns,
--- config/settings.py
+++ config/settings.py
@@ -144,7 +144,7 @@ AUTHENTICATION_BACKENDS = (
     'allauth.account.auth_backends.AuthenticationBackend',
 )
 SITE_ID = 1
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/accounts/'
 
 SOCIALACCOUNT_ADAPTER = 'accounts.adapter.SocialAccountAdapter'
 
