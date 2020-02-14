145d79f48ee00b0289f36495f538fa33897e9799
정승현 <fm1003@naver.com>
2020-02-15 02:42:28 +0900

    Auto stash before merge of "feat/kasa" and "origin/templates/detail"

---
+++ Kasa/static/javascript/main.js
@@ -0,0 +1,6 @@
+$('#searchbox').hover(function () {
+    alert('tt')
+    // $('#main_wrap_2').css({'background': 'linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5))'});
+}, function () {
+    $('#main_wrap_2').css({'background': 'transparent'})
+});
