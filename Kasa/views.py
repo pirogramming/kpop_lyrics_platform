92a85f6f28ae3217e2f3286f1df5491009d0cedf
pot1223 <pot1223@naver.com>
2020-02-17 15:12:26 +0900

    before merged

--- Kasa/views.py
+++ Kasa/views.py
@@ -1,7 +1,5 @@
 import json
 import random
-import re
-
 import requests
 from bs4 import BeautifulSoup
 from django.http import HttpResponse
@@ -9,8 +7,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from Kasa.convert_url import convert_youtube
 from Kasa.models import *
 from django.db.models import Q
-
-from accounts.decorators import login_required
+from django.core.paginator import Paginator
 
 
 def song_detail(request, song_pk):
@@ -94,8 +91,6 @@ def enter_all_lyrics(request, song_id):
                 location = all_lang_dict.get(str(index + 1), None)
                 location['rom'] = lyrics
 
-        print(all_lang_dict)
-
         context = {
             'song': song,
             'singers': singers,
@@ -109,7 +104,6 @@ def enter_all_lyrics(request, song_id):
 
 def modify_and_create_each_lyrics(request, song_id):
     if request.method == "POST":
-        print(request.POST)
         request_dict = request.POST
         song = Songs.objects.get(pk=song_id)
         singers = song.singer.all()
@@ -117,7 +111,6 @@ def modify_and_create_each_lyrics(request, song_id):
         length = int(request.POST['length'])
         if all_lyrics:
             # DB에 저장된 노래가사들이 있을 경우
-            print('있음')
             existing_lyrics_length = len(all_lyrics)
             if existing_lyrics_length > length:
                 # 새로 들어온 가사가 기존의 가사의 길이보다 작을 경우 삭제
@@ -182,15 +175,15 @@ def modify_and_create_each_lyrics(request, song_id):
 def search(request):
     kwd = request.GET.get('kwd', None)
     if not kwd:
-        # noresult = True
-        noresult = False
+        noresult = True
 
         context = {
             'kwd': kwd,
             'noresult': noresult,
-            'albums': Albums.objects.all(),
         }
         return render(request, 'Kasa/search_detail.html', context)
+
+    target = request.GET.get('target', '')
 
     singers_list = []
     songs_list = []
@@ -204,55 +197,141 @@ def search(request):
     albums = Albums.objects.filter(aname__icontains=kwd)
     groups = Groups.objects.filter(Q(gname__icontains=kwd) | Q(agency__icontains=kwd))
 
-    if len(singers) > 5:
-        for singers_count in range(5):
-            singers_list.append(singers[singers_count])
-    else:
-        singers_list = singers
+    if singers:
+        for singer in singers:
+            singers_list.append(singer)
+    if songs:
+        for song in songs:
+            songs_list.append(song)
+    if albums:
+        for album in albums:
+            albums_list.append(album)
+    if groups:
+        for group in groups:
+            groups_list.append(group)
+    if lyrics:
+        for overlap_check in lyrics:
+            if overlap_check in lyrics_list:
+                continue
+            lyrics_list.append(overlap_check)
 
-    if len(songs) > 5:
-        for songs_count in range(5):
-            songs_list.append(songs[songs_count])
-    else:
-        songs_list = songs
+    # 그룹을 검색했을 때 해당 그룹의 앨범,노래,가수
+    if groups:
+        for group in groups:
+            for group_singer in group.group_singer.all():
+                singers_list.append(group_singer)
+            for group_album in group.group_album.all():
+                albums_list.append(group_album)
+                for group_song in group_album.album_song.all():
+                    songs_list.append(group_song)
 
-    if len(albums) > 5:
-        for albums_count in range(5):
-            albums_list.append(albums[albums_count])
-    else:
-        albums_list = albums
+    # 가수를 검색했을 때 해당 가수의 그룹,앨범
+    if singers:
+        for singer in singers:
+            for singer_group in singer.group.all():
+                groups_list.append(singer_group)
+                for singer_album in singer_group.group_album.all():
+                    albums_list.append(singer_album)
 
-    if len(groups) > 5:
-        for groups_count in range(5):
-            groups_list.append(groups[groups_count])
-    else:
-        groups_list = groups
+    # 앨범을 검색했을 때 해당 앨범의 그룹,노래
+    if albums:
+        for album in albums:
+            groups_list.append(album.group)
+            for album_song in album.album_song.all():
+                songs_list.append(album_song)
 
-    for overlap_check in lyrics:
-        if overlap_check in lyrics_list:
-            continue
-        if len(lyrics) > 5:
-            for lyrics_count in range(5):
-                lyrics_list.append(lyrics[overlap_check])
-        else:
-            lyrics_list.append(lyrics[lyrics_count])
+    # 노래를 검색했을 때 해당 노래의 앨범과 그룹
+    if songs:
+        for song in songs:
+            if song.album in albums_list:
+                pass
+            else:
+                albums_list.append(song.album)
+            if song.album.group in groups_list:
+                pass
+            else:
+                groups_list.append(song.album.group)
 
-    if len(singers_list) <= 0 and len(songs_list) <= 0 and len(lyrics_list) <= 0 \
-            and len(albums_list) <= 0 and len(groups_list) <= 0:
-        noresult = False
+    artists_list = groups_list + singers_list
+
+    if len(singers_list) <= 0 and len(artists_list) <= 0 and len(lyrics_list) <= 0 \
+            and len(albums_list) <= 0:
+        noresult = True
         context = {
             'kwd': kwd,
             'noresult': noresult,
+            'artist_length': 0,
         }
         return render(request, 'Kasa/search_detail.html', context)
 
+    double_prev_page = None
+    double_next_page = None
+
+    # Artist 페이지네이션
+    artists_length = len(artists_list)
+    artists_paginator = Paginator(artists_list, 6)
+    if request.GET.get('target') == 'artist':
+        if request.GET.get('artist_page'):
+            present_page = int(request.GET.get('artist_page'))
+            if present_page - 2 > 1:
+                double_prev_page = present_page - 2
+            if present_page + 2 < artists_paginator.num_pages:
+                double_next_page = present_page + 2
+    artist_post = request.GET.get('song_page', 1)
+    artists_posts = artists_paginator.get_page(artist_post)
+
+    # Song 페이지네이션
+    songs_length = len(songs_list)
+    songs_paginator = Paginator(songs_list, 10)
+    if request.GET.get('target') == 'song':
+        if request.GET.get('song_page'):
+            present_page = int(request.GET.get('song_page'))
+            if present_page - 2 > 1:
+                double_prev_page = present_page - 2
+            if present_page + 2 < songs_paginator.num_pages:
+                double_next_page = present_page + 2
+    song_post = request.GET.get('song_page', 1)
+    songs_posts = songs_paginator.get_page(song_post)
+
+    # Album 페이지네이션
+    albums_length = len(albums_list)
+    albums_paginator = Paginator(albums_list, 6)
+    if request.GET.get('target') == 'album':
+        if request.GET.get('album_page'):
+            present_page = int(request.GET.get('album_page'))
+            if present_page - 2 > 1:
+                double_prev_page = present_page - 2
+            if present_page + 2 < albums_paginator.num_pages:
+                double_next_page = present_page + 2
+    album_post = request.GET.get('album_page', 1)
+    albums_posts = albums_paginator.get_page(album_post)
+
+    # Lyric 페이지네이션
+    lyrics_length = len(lyrics_list)
+    lyrics_paginator = Paginator(lyrics_list, 5)
+    if request.GET.get('target') == 'lyric':
+        if request.GET.get('lyric_page'):
+            present_page = int(request.GET.get('lyric_page'))
+            if present_page - 2 > 1:
+                double_prev_page = present_page - 2
+            if present_page + 2 < lyrics_paginator.num_pages:
+                double_next_page = present_page + 2
+    lyric_post = request.GET.get('lyric_page', 1)
+    lyrics_posts = lyrics_paginator.get_page(lyric_post)
+
     context = {
         'kwd': kwd,
-        'singers': singers_list,
-        'songs': songs_list,
-        'lyrics': lyrics_list,
-        'albums': albums_list,
-        'groups': groups_list,
+        'artists': artists_posts,
+        'songs': songs_posts,
+        'albums': albums_posts,
+        'lyrics': lyrics_posts,
+        'artists_length': artists_length,
+        'songs_length': songs_length,
+        'albums_length': albums_length,
+        'lyrics_length': lyrics_length,
+        'double_prev_page': double_prev_page,
+        'double_next_page': double_next_page,
+        'target': target,
     }
     return render(request, 'Kasa/search_detail.html', context)
 
@@ -288,7 +367,6 @@ def album_detail(request, album_pk):
     # a = str(items)
     # prd_names = re.sub('<.+?>', '', a, 0).strip()
     # print(prd_names)
-    print(album.content)
     context = {
         'album': album,
         'items': items,
@@ -327,22 +405,52 @@ def pick_one_group_by_user(user=None):
         interests = interests.replace('/', ' ')
         interests = interests.split(' ')
         group_list = []
+        kwd_group_list = []
+        choice_group = None
         for interest in interests:
             if not interest:
                 continue
             sets = Groups.objects.filter(gname__icontains=interest)
             if not sets:
-                groups = Groups.objects.all()
-                if groups:
-                    return random.choice(groups)
-                return None
+                continue
             for query in sets:
-                group_list.append(query)
-            return random.choice(group_list)
+                kwd_group_list.append(query)
+        if kwd_group_list:
+            group = random.choice(kwd_group_list)
+            while not group.group_image:
+                kwd_group_list.remove(group)
+                if not kwd_group_list:
+                    group = None
+                    break
+                group = random.choice(kwd_group_list)
+            choice_group = group
+            if choice_group:
+                return choice_group
+
+        group_list = list(Groups.objects.all())
+        if not group_list:
+            return None
+
+        group = random.choice(group_list)
+        while not group.group_image:
+            group_list.remove(group)
+            if not group_list:
+                return None
+            group = random.choice(group_list)
+        return group
+
     else:
         groups = Groups.objects.all()
+        groups = list(groups)
+
         if groups:
-            return random.choice(groups)
+            group = random.choice(groups)
+            while not group.group_image:
+                groups.remove(group)
+                if not groups:
+                    return None
+                group = random.choice(groups)
+            return group
         return None
 
 
@@ -351,6 +459,6 @@ def main(request):
     group = pick_one_group_by_user(request.user)
     context = {
         'five_songs': five_songs,
-        'group': group
+        'group': group,
     }
     return render(request, 'Kasa/main.html', context)
